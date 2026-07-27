f56042fb2d60cf1325398cdf6b4d9c78f8864585
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-07-27 14:28:57 +0300

    add components and style files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route } from "react-router";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Route index element={<App />} />
+    <Route path="/variables" element={<VariablesList />} />
+    <Route path="/variables/:variableId" element={<VariableDetail />} />
+  </BrowserRouter>,
+);
