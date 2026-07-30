e64b9993ab02e58fc951494e2e049e7fee1cbf11
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-07-30 15:44:26 +0300

    add additional types

--- src/types/vinServices.ts
+++ src/types/vinServices.ts
@@ -1,6 +1,6 @@
-interface VinResults {
-  Value: string;
-  ValueId: string;
+interface DecodeVinResult {
+  Value: string | null;
+  ValueId: string | null;
   Variable: string;
   VariableId: number;
 }
@@ -9,5 +9,21 @@ export interface VinDecodeResponse {
   Count: number;
   Message?: string;
   SearchCriteria: string;
-  Results: VinResults[];
+  Results: DecodeVinResult[];
 }
+
+export interface VehicleVariable {
+  ID: number;
+  Name: string;
+  Description: string;
+}
+
+export interface VariableListResponse {
+  Count: number;
+  Message?: string;
+  Results: VehicleVariable[];
+}
+
+//? Variable і Value з масиву Results у якиї Value заповнено
+//? / variables Список усіх можливих змінних з описами
+//? /variables/{variableId} опис конкретної змінної
