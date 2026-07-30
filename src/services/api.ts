75dc51bc0e69cb8bf51a150f7daeee1d523f3c88
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-07-30 15:44:52 +0300

    add fetch requests logic

---
+++ src/services/api.ts
@@ -0,0 +1,17 @@
+import type { VinDecodeResponse } from "../types/vinServices";
+
+const BASE_URL = "https://vpic.nhtsa.dot.gov/api";
+
+// Запит для декодування VIN номеру
+export async function decodeVin(vin: string): Promise<VinDecodeResponse> {
+  const res = await fetch(`${BASE_URL}/vehicles/decideVIN/${vin}?format=json`);
+  return res.json();
+}
+
+export async function getVehicleVariableList() {
+  const res = await fetch(
+    `${BASE_URL}/vehicles/getvehiclevariablelist?format=json`,
+  );
+
+  return res.json();
+}
